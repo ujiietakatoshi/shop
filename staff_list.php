72b5de0a8376886df29eb50eddf6244f45879980
ujiietakatoshi <fadeoutinu@gmail.com>
2020-03-24 00:09:28 +0900

    initial commit

--- staff_list.php
+++ staff_list.php
@@ -17,7 +17,7 @@
   $dbh=new PDO($dsn,$user,$password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-  $sql='SELECT name FROM mst_staff WHERE 1';
+  $sql='SELECT code,name FROM mst_staff WHERE 1';
   $stmt=$dbh->prepare($sql);
   $stmt->execute();
 
@@ -25,6 +25,7 @@
 
   print'スタッフ一覧<br /><br />';
 
+  print'<form method="post"action="staff_edit.php">';
   while(true)
   {
     $rec=$stmt->fetch(PDO::FETCH_ASSOC);
@@ -32,9 +33,12 @@
     {
     break;
     }
+    print'<input type="radio"name="staffcode"value="'.$rec['code'].'">';
     print$rec['name'];
     print'<br />';
   }
+  print'<input type="submit"value="修正">';
+  print'</form>';
   
 }
 catch(Exception $e)
