14d8b0fe5ecf88010bb94cf5c704fb8edac473b8
ujiietakatoshi <fadeoutinu@gmail.com>
2020-03-24 23:10:45 +0900

    commit5

---
+++ staff_delete_done.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>野菜市場</title>
+  </head>
+  <body>
+
+  <?php
+
+  try
+  {
+  
+  $staff_code=$_POST['code'];
+
+
+  $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+  $user='root';
+  $password='';
+  $dbh=new PDO($dsn,$user,$password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+  $sql='DELETE FROM mst_staff WHERE code=?';
+  $stmt=$dbh->prepare($sql);
+  $data[]=$staff_code;
+  $stmt->execute($data);
+
+  $dbh=null;
+
+
+  }
+  catch(Exception $e)
+  {
+    print'ただいま障害により大変ご迷惑をおかけしております。';
+    exit();
+  }
+
+  ?>
+  削除しました。<br />
+  <br />
+  <a href="staff_list.php">戻る</a>
+
+  </body>
+</html>
