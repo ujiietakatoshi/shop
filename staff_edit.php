26b675a5f0b2390e8c6c9b35fe3a20edbbf028e2
ujiietakatoshi <fadeoutinu@gmail.com>
2020-03-24 21:16:30 +0900

    commit1

--- staff_edit.php
+++ staff_edit.php
@@ -11,46 +11,51 @@
   try
   {
   
+  $staff_code=$_POST['staffcode'];
+
   $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
   $user='root';
   $password='';
   $dbh=new PDO($dsn,$user,$password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-  $sql='SELECT code,name FROM mst_staff WHERE 1';
+  $sql='SELECT name FROM mst_staff WHERE code=?';
   $stmt=$dbh->prepare($sql);
-  $stmt->execute();
+  $data[]=$staff_code;
+  $stmt->execute($data);
+
+  $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+  $staff_name=$rec['name'];
 
   $dbh=null;
 
-  print'スタッフ一覧<br /><br />';
-
-  print'<form method="post"action="staff_edit.php">';
-  while(true)
-  {
-    $rec=$stmt->fetch(PDO::FETCH_ASSOC);
-    if($rec==false)
-    {
-    break;
-    }
-    print'<input type="radio"name="staffcode"value="'.$rec['code'].'">';
-    print$rec['name'];
-    print'<br />';
   }
-  print'<input type="submit"value="修正">';
-  print'</form>';
-  
-}
-catch(Exception $e)
-{
-  print'ただいま障害により大変ご迷惑をおかけしております。';
+  catch(Exception $e)
+  {
+    print'ただいま障害により大変ご迷惑をおかけしております。';
     exit();
-}
-
+  }
 
   ?>
 
-  <a href="staff_list.php">戻る</a>
+  スタッフ修正<br />
+  <br />
+  スタッフコード<br />
+  <?php print$staff_code;?>
+  <br />
+  <br />
+  <form method="post" action="staff_edit_check.php">
+  <input type="hidden" name="code" value="<?php print $staff_code;?>">
+  スタッフ名<br />
+  <input type="text" name="name" style="width:200px" value="<?php print $staff_name;?>"><br />
+  パスワードを入力して下さい。<br />
+  <input type="password"name="pass"style="width:100px"><br />
+  パスワードをもう１度入力して下さい。<br />
+  <input type="password"name="pass2"style="width:100px"><br />
+  <br />
+  <input type="button"onclick="history.back()"value="戻る">
+  <input type="submit"value="OK">
+  </form>
 
   </body>
 </html>
